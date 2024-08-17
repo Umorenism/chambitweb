12bff60ae832899efc1f8eca5ea6268329fb38e4
Umorenism <umorenismvic@gmail.com>
2024-08-16 22:54:24 -0700

    upated

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import pic from "../public/imageLogo/pic1.jpg";
-import pic2 from "../public/imageLogo/pic2.jpg";
 import pic3 from "../public/imageLogo/pic3.jpg";
-import pic4 from "../public/imageLogo/pic4.jpg";
+
+import pic5 from "../public/imageLogo/logo.jpg";
 const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center  px-4 flex-col bg-slate-100">
-      <h1 className="text-2xl font-bold mb-4 md:text-4xl text-green-700 uppercase underline">
+      <h1 className="text-2xl py-4 font-bold mb-4 md:text-4xl text-green-700 uppercase underline">
         Concierge Service
       </h1>
 
       <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col md:flex-row">
         <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
-          <img src={pic} className="w-full" />
+          <img src={pic5} className="w-full" />
         </div>
-        <div className="md:w-[80%] justify-center flex flex-col w-full">
+        <div className="md:w-[80%] ml-2 justify-center flex flex-col w-full">
           <h1 className="text-3xl">Name:Pam Chambit</h1>
           <h2 className="text-xl py-2">
-            Telegram Username:<a href="https://www.telegram.com">Pam_berry</a>
+            https:
+            <a
+              href="https://t.me/Pam_berry"
+              className="text-blue-600 underline"
+            >
+              @Pam_berry
+            </a>
           </h2>
-          <p>Official:Concierge Service</p>
+          <p>Role:Officia Listing Agent</p>
         </div>
       </div>
       <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
         <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
-          <img src={pic2} className="w-full" />
+          <img src={pic5} className="w-full" />
         </div>
         <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
-          <h1 className="text-3xl">Name:Pam Chambit</h1>
+          <h1 className="text-3xl">Name:Senjay</h1>
           <h2 className="text-xl">
-            Telegram Username:<a href="https://www.telegram.com">Pam_berry</a>
+            https:
+            <a href="https://t.me/sayjay" className="text-blue-600 underline">
+              @sayjay
+            </a>
           </h2>
-          <p>Official:Concierge Service</p>
+          <p>Role:Officia Listing Agent</p>
         </div>
       </div>
       <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
@@ -38,23 +46,86 @@ const App = () => {
           <img src={pic3} className="w-full" />
         </div>
         <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
-          <h1 className="text-3xl">Name:Pam Chambit</h1>
+          <h1 className="text-3xl">Name:Kama rochambit</h1>
           <h2 className="text-xl">
-            Telegram Username:<a href="https://www.telegram.com">Pam_berry</a>
+            https:
+            <a
+              href="https://t.me/kamarochambit"
+              className="text-blue-600 underline"
+            >
+              @kamarochambit
+            </a>
           </h2>
-          <p>Official:Concierge Service</p>
+          <p>Role:Officia Listing Agent</p>
         </div>
       </div>
       <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
         <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
-          <img src={pic4} className="w-full" />
+          <img src={pic5} className="w-full" />
         </div>
         <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
-          <h1 className="text-3xl">Name:Pam Chambit</h1>
+          <h1 className="text-3xl">Kyra Totler</h1>
           <h2 className="text-xl">
-            Telegram Username:<a href="https://www.telegram.com">Pam_berry</a>
+            https:
+            <a
+              href="https://t.me/kyratotler"
+              className="text-blue-600 underline"
+            >
+              @kyratotler
+            </a>
           </h2>
-          <p>Official:Concierge Service</p>
+          <p>Role:Officia Listing Agent</p>
+        </div>
+      </div>
+      <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
+        <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
+          <img src={pic5} alt="logo" className="w-full" />
+        </div>
+        <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
+          <h1 className="text-3xl">Zohiabussian</h1>
+          <h2 className="text-xl">
+            https:
+            <a
+              href="https://t.me/zohaibhusssian"
+              className="text-blue-600 underline"
+            >
+              @Zohiabhusssian
+            </a>
+          </h2>
+          <p>Role:Officia Listing Agent</p>
+        </div>
+      </div>
+      <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
+        <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
+          <img src={pic5} alt="logo" className="w-full" />
+        </div>
+        <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
+          <h1 className="text-3xl">Vimzy</h1>
+          <h2 className="text-xl">
+            https:
+            <a href="https://t.me/vimzy05" className="text-blue-600 underline">
+              @vimzy05
+            </a>
+          </h2>
+          <p>Role:Officia Listing Agent</p>
+        </div>
+      </div>
+      <div className="bg-white shadow-lg py-2 rounded-md w-full max-w-[1100px] flex px-4 flex-col mt-4 md:flex-row">
+        <div className="md:w-[20%] aspect-video h-auto  py-4 w-full">
+          <img src={pic5} alt="logo" className="w-full" />
+        </div>
+        <div className="md:w-[80%] justify-center flex flex-col w-full ml-2">
+          <h1 className="text-3xl">ARCHIBACO</h1>
+          <h2 className="text-xl">
+            https:
+            <a
+              href="https://t.me/ARCHIBACO"
+              className="text-blue-600 underline"
+            >
+              @ARCHIBACO
+            </a>
+          </h2>
+          <p>Role:Officia Listing Agent</p>
         </div>
       </div>
     </div>
